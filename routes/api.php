1152d336439b66d948633071d4caf2db7c22d3a3
elhadiabdallah <3arafa109@gmail.com>
2021-10-16 22:38:45 +0100

    test to upload at hosting

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/test', function (Request $request) {
+    $data = [
+        'status' => 200,
+        'msg' => "لقد تمّ رفع الموقع بنجاح"
+    ];
+    return response()->json($data, 200);
+});
