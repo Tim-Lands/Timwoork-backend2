4de0e6eea5eb0e8fc12e3325e2bb9fe446d318c1
arouiTarek <yassin.tarek77r@gmail.com>
2021-12-10 14:31:50 +0100

    add user in product and make factory galaries

---
+++ database/factories/GalaryFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Galary;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class GalaryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Galary::class;
+
+    public function definition()
+    {
+        $image = $this->faker->image(storage_path('app/products/galaries-images'), 640, 480, null, false);
+        // $file =  \Illuminate\Http\UploadedFile::fake()->create('test.pdf')->store('products/galaries-file');
+        return [
+            'path' => $image,
+            'full_path' => $image,
+            'size' => $this->faker->randomFloat($nbMaxDecimals = NULL, $min = 0.1, $max = 2) . ' mb',
+            'url_video' => 'https://www.youtube.com/watch?v=B17oiTBZCvc',
+            'type_file' => 'image',
+            'mime_type' => 'png'
+        ];
+    }
+}
