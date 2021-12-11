2d7fba7eb99905524e0568c0161d496b19417fd3
elhadiabdallah <3arafa109@gmail.com>
2021-12-11 22:22:35 +0100

    refactor filter and rating

--- app/Http/Controllers/Product/RatingController.php
+++ app/Http/Controllers/Product/RatingController.php
@@ -21,43 +21,84 @@ class RatingController extends Controller
     public function rate($id, RatingStoreRequest $request)
     {
         //id  جلب العنصر بواسطة
-        $product = Product::find($id);
+        $product = Product::withAvg('ratings', 'rating')->find($id);
+        // جلب المستخدم الحالي
+        $user_id = Auth::id();
         // شرط اذا كان العنصر موجود
         if (!$product || !is_numeric($id))
             // رسالة خطأ
             return response()->error('هذا العنصر غير موجود', 403);
-
-        $user_id = Auth::id();
+        // قم بجلب تقييم الخدمة من طرف المستخدم الحالي 
         $rate = Rating::where('user_id', $user_id)->where('product_id', $product->id)->first();
+
+        // إذا كان التقييم موجود وغير فارغ يمكن التعديل عليه
         if (!empty($rate)) {
-            $rate->rating = $request->rating;
-            $rate->comment = $request->comment;
+
             try {
+                // إعطاء قيمة جديدة للتقييم والتعليق
+                $rate->rating = $request->rating;
+                $rate->comment = $request->comment;
                 DB::beginTransaction();
+                // تحديث المعلومات
                 $rate->update();
+                // جلب الخدمة من جديد لتحديث حقل متوسط التقييمات
+                $product = $this->getRatedProduct($rate->id);
+                // إعطاء قيمة جديدة لمتوسط التقييمات
+                $product->ratings_avg = $product->ratings_avg_rating;
+                // حفظ القيمة الجديدة لمعدل التقييمات
+                $product->save();
                 DB::commit();
+                // إرسال رسالة تفيد بنجاح الأمر
                 return response()->success('لقد تمّ التعديل على التقييم بنجاح', $rate);
             } catch (Exception $ex) {
+                // وإلا قم بالتراجع عن التغيير في حالة حدوث خطأ
                 DB::rollback();
-                return $ex;
+                //return $ex;
+                // وإرسال رسالة تفيد بوجود خطأ غير متوقع
                 return response()->error('هناك خطأ ما حدث في قاعدة بيانات , يرجى التأكد من ذلك', 403);
             }
         } else {
+            // في حالة عدم وجود تقييم لهذه الخدمة من طرف المستخدم الحالي
             try {
                 DB::beginTransaction();
+                // قم بإنشاء تقييم جديد
                 $rating = Rating::create([
                     'user_id' => $user_id,
                     'product_id' => $product->id,
                     'rating' => $request->rating,
                     'comment' => $request->comment
                 ]);
+                // ثم جلب الخدمة من جديد لتعديل الحقلين : عدد التقييمات ومعدل التقييمات
+                $product = $this->getRatedProduct($rating->id);
+                $product->increment('ratings_count');
+                $product->ratings_avg = $product->ratings_avg_rating;
+                // حفظ التعديلات الجديدة على الخدمة
+                $product->save();
+
                 DB::commit();
+                // إرسال رسالة النجاح
                 return response()->success('لقد تمّ إضافة  التقييم بنجاح', $rating);
             } catch (Exception $ex) {
+                // في حالة الخطأ يتم التراجع عن أي تغيير حدث في قاعدة البيانات
                 DB::rollback();
-                return $ex;
+                //return $ex;
+                // ثم إرسال رسالة الخطأ
                 return response()->error('هناك خطأ ما حدث في قاعدة بيانات , يرجى التأكد من ذلك', 403);
             }
         }
+    }
+
+    /**
+     * دالة لجلب الخدمة التي تم تقييمها 
+     *
+     * @param  $rating_id
+     */
+    public function getRatedProduct($rating_id)
+    {
+        return Product::select('id')->whereHas('ratings', function ($q) use ($rating_id) {
+            $q->where('id', $rating_id);
+        })
+            ->withAvg('ratings', 'rating')
+            ->first();
     }
 }
