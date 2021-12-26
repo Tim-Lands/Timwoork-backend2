a9ec60486b91399e851bf0207a94cd3bc22a8c77
elhadiabdallah <3arafa109@gmail.com>
2021-12-26 13:55:49 +0100

    refactor Dark&Light Modes

--- app/Http/Controllers/Auth/DarkModeController.php
+++ app/Http/Controllers/Auth/DarkModeController.php
@@ -5,18 +5,21 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DarkModeController extends Controller
 {
-    public function lightMode(User $user)
+    public function lightMode()
     {
+        $user = Auth::user();
         $user->profile->dark_mode = false;
         $user->profile->save();
         return response()->success('لقد تم تغيير الوضع إلى وضع نهاري');
     }
 
-    public function darkMode(User $user)
+    public function darkMode()
     {
+        $user = Auth::user();
         $user->profile->dark_mode = true;
         $user->profile->save();
         return response()->success('لقد تم تغيير الوضع إلى وضع ليلي');
