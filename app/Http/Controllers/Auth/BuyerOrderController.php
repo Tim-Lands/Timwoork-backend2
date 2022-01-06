99eed83a89862f2cba8cbc2f814c9a3963392c17
elhadiabdallah <3arafa109@gmail.com>
2022-01-06 21:10:29 +0100

    again hhhhhh wip

--- app/Http/Controllers/Auth/BuyerOrderController.php
+++ app/Http/Controllers/Auth/BuyerOrderController.php
@@ -18,8 +18,12 @@ class BuyerOrderController extends Controller
     public function __invoke(Request $request)
     {
         $paginate = $request->query('paginate') ? $request->query('paginate') : 10;
-        $buyer = Auth::user()->profile->profile_seller()->id;
-        $items = Item::with(['profileSeller.profile.user'])->where('profile_seller_id', $buyer)->paginate($paginate);
+        $buyer = Auth::id();
+        $items = Item::with(['profileSeller.profile.user', 'order' => function ($q) use ($buyer) {
+            $q->with(['cart' => function ($q) use ($buyer) {
+                $q->where('user_id', $buyer);
+            }]);
+        }])->paginate($paginate);
         return response()->success('لقد تم جلب مشترياتك بنجاح', $items);
     }
 }
