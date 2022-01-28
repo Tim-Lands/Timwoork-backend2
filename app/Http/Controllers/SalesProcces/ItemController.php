279b472d8a5eb4976f4b62121eb988ea6ce56ea8
elhadiabdallah <3arafa109@gmail.com>
2022-01-28 18:00:46 +0100

    wip

--- app/Http/Controllers/SalesProcces/ItemController.php
+++ app/Http/Controllers/SalesProcces/ItemController.php
@@ -35,7 +35,7 @@ class ItemController extends Controller
     {
         // جلب الطلبية
         $item = Item::whereId($id)
-            ->with(['order.cart.user.profile', 'profileSeller.profile', 'item_rejected', 'item_modified', 'attachments', 'conversation'])
+            ->with(['order.cart.user.profile', 'profileSeller.profile', 'item_rejected', 'item_modified', 'attachments', 'conversation.messages.user.profile', 'conversation.messages.attachments'])
             ->first();
         if (!$item) {
             // رسالة خطأ
