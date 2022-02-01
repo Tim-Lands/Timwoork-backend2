8a1e52e86816c0f4b9a6dc2531cf95b0d59dc7d0
elhadiabdallah <3arafa109@gmail.com>
2022-02-01 19:14:01 +0100

    wip

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -52,7 +52,7 @@ class Conversation extends Model
      */
     public function latest_msg(): HasMany
     {
-        return $this->messages()->order_by('id', 'desc')->first();
+        return $this->messages()->first();
     }
 
 
