16add2b7062ad6541a761a52e9aabc3cbd6ae084
elhadiabdallah <3arafa109@gmail.com>
2022-02-01 19:35:58 +0100

    wip

--- app/Http/Controllers/ConversationController.php
+++ app/Http/Controllers/ConversationController.php
@@ -27,8 +27,8 @@ class ConversationController extends Controller
         $user = Auth::user();
         $conversations = Conversation::with(['members' => function ($query) use ($user) {
             $query->where('user_id', $user->id);
-        }, 'messages' => function (Builder $q) {
-            $q->first()->latest();
+        }, 'messages' => function ($q) {
+            $q->latest()->first();
         }])->withCount(['messages' => function (Builder $query) {
             $query->whereNull('read_at');
         }])->paginate($paginate);
