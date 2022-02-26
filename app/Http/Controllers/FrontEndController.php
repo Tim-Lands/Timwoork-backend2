e0a7c87d29bd34269ee8a3a057b58901dd33c5b0
Aroui Tarek <yassin.tarek77r@gmail.com>
2022-02-26 18:17:47 +0100

    change in contact us and display products

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ContactRequest;
 use App\Models\Category;
-
+use App\Models\Contact;
 use App\Models\Product;
+use Carbon\Carbon;
+use Exception;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 
 class FrontEndController extends Controller
 {
@@ -147,5 +152,100 @@ class FrontEndController extends Controller
 
         // اظهار العناصر
         return response()->success(__("messages.oprations.get_all_data"), $categories);
+    }
+
+    /**
+     * get_products_by_subcategory => جلب الخدمات التابعة لهذا التصنيف
+     *
+     * @param  mixed $id
+     * @return void
+     */
+    public function get_products_by_subcategory($id)
+    {
+        // جلب التصنيف الرئيسي من اجل التحقق
+        $subcatagory = Category::select('id', 'name_ar', 'name_en', 'name_fr', 'parent_id', 'icon')
+        ->whereId($id)
+        ->orWhere('slug', $id)
+        ->child()
+        ->with(['products' => function ($q) {
+            $q->select('id', 'profile_seller_id', 'category_id', 'title', 'price', 'thumbnail', 'count_buying', 'duration')
+            ->where('is_completed', 1)
+            ->where('status', 1)
+            ->where('is_active', 1)
+            ->where('is_vide', 0)
+            ->with('profileSeller', function ($q) {
+                $q->select('id', 'profile_id')->without('level', 'badge')
+                ->with('profile', function ($q) {
+                    $q->select('id', 'first_name', 'last_name', 'user_id')
+                    ->with('user:id,username')->without('level', 'badge');
+                });
+            });
+        }])->first();
+        // التحقق من التصنيف انه موجود
+        if (!$subcatagory) {
+            return response()->error(__("messages.errors.element_not_found"), 403);
+        }
+        // اظهار العناصر
+        return response()->success(__("messages.oprations.get_data"), $subcatagory);
+    }
+
+
+
+    /**
+     * send_to_dashboad => دالة ارسال الرسالة الى لوحة التحكم
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function send_to_dashboad(ContactRequest $request)
+    {
+        try {
+            $contact = Contact::selection()
+                                        ->where(function ($query) use ($request) {
+                                            $query->where('email', $request->email)
+                                                  ->orWhere('ip_client', $request->ip());
+                                        })
+                                        ->where('date_expired', '>', Carbon::now()->toDateTimeString())
+                                        ->first();
+            if ($contact) {
+                return response()->error(__("messages.contact.cannot_sent_before_48"), Response::HTTP_BAD_REQUEST);
+            }
+            // انشاء مصفوفة من اجل ارسال المعلومات
+            $data_contact = [
+                "subject"         => $request->subject,
+                "email"           => $request->email,
+                "full_name"       => $request->full_name,
+                "type_message"    => $request->type_message,
+                "message"         => $request->message,
+                "date_expired"    => Carbon::now()->addDays(2)->toDateTimeString(),
+                "ip_client"       => $request->ip()
+            ];
+
+            // شرط اذا كان يوجد رابط
+            if ($request->has('url')) {
+                // شرط ان يكون الرابط يحتوي على غوغل درايف او دروب بوكس
+                if (str_contains($request->url, Contact::URL_GOOGLE_DRIVE) || str_contains($request->url, Contact::URL_DROPBOX)) {
+                    $data_contact["url"] = $request->url;
+                } else {
+                    // رسالة خطأ
+                    return response()->error(__("messages.contact.not_found_url"), Response::HTTP_BAD_REQUEST);
+                }
+            }
+            /* ------------------------------- عملية ارسال ------------------------------ */
+
+            DB::beginTransaction();
+            // عملية انشاء الرسالة
+            Contact::create($data_contact);
+            // ارسال اشعار للوحة التحكم
+            // انهاء المعاملة بشكل جيد :
+            DB::commit();
+
+            // رسالة نجاح العملية
+            return response()->success(__("messages.contact.success_message_contact"));
+        } catch (Exception $ex) {
+            return $ex;
+            DB::rollBack();
+            return response()->error(__("messages.errors.error_database"), Response::HTTP_FORBIDDEN);
+        }
     }
 }
