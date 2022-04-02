19413d6e899d0a70f8f01203606ba2c883af8000
elhadiabdallah <3arafa109@gmail.com>
2022-04-02 22:31:44 +0100

    tset

--- database/migrations/2022_01_06_211221_create_notifications_table.php
+++ database/migrations/2022_01_06_211221_create_notifications_table.php
@@ -15,7 +15,7 @@ class CreateNotificationsTable extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
-            $table->uuid('id')->primary();
+            $table->uuid('uuid')->primary();
             $table->string('type');
             $table->morphs('notifiable');
             $table->text('data');
