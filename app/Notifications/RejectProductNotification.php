30890ad7b94e57154bbb2f41c5da27f30b549094
Aroui Tarek <yassin.tarek77r@gmail.com>
2022-05-11 03:06:45 +0100

    fixed problem in mail

--- app/Notifications/RejectProductNotification.php
+++ app/Notifications/RejectProductNotification.php
@@ -33,7 +33,7 @@ class RejectProductNotification extends Notification
      */
     public function via($notifiable)
     {
-        return ['database'];
+        return ['mail','database'];
     }
 
     /**
