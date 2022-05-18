169804d5593a407f90cbad76b607d1786efc64dd
Aroui Tarek <yassin.tarek77r@gmail.com>
2022-05-18 21:50:30 +0100

    fixed problem in xss

--- app/Http/Middleware/HandelXss.php
+++ app/Http/Middleware/HandelXss.php
@@ -21,7 +21,7 @@ class HandelXss
         $input = $request->all();
 
         array_walk_recursive($input, function (&$input) {
-            $input = strip_tags($input);
+            $input = strip_tags($input, '<p><a><b><i><u><strong><em><ul><ol><li><h1><h2><h3><h4><h5><h6><div><span><img><iframe><embed><object><param><video><audio><source><track><map><area><canvas><svg><table><caption><tbody><thead><tfoot><col><colgroup><tr><td><th><form><fieldset><legend><label><input><textarea><select><optgroup><option><button><datalist><output><progress><meter><details><summary><menu><menuitem><pre><code><blockquote><cite><q><del><ins><time><mark><small><big><sub><sup><bdo><button>');
         });
 
         $request->merge($input);
