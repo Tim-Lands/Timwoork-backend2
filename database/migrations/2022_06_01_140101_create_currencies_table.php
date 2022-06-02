2c16b92db3cb26d9dc2bf3013124f55de0506250
sharaf eldeen <eldeenshraf@gmail.com>
2022-06-02 14:27:25 +0200

    migrate

--- database/migrations/2022_06_01_140101_create_currencies_table.php
+++ database/migrations/2022_06_01_140101_create_currencies_table.php
@@ -15,7 +15,8 @@ class CreateCurrenciesTable extends Migration
     {
         Schema::create('currencies', function (Blueprint $table) {
             $table->timestamps();
-            $table->char('code', 5)->primary();
+            $table->char('code', 5);
+            $table->primary('code');
             $table->string('name', 25)->nullable(false);
             $table->char('symbol', 5)->nullable(false);
             $table->char('symbol_native', 5)->nullable(false);
