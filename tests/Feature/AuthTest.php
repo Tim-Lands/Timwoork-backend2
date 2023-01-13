fc3a166f7776550daf2ea2894eac239fe3b6dbbd
Sharaf-eldeen <eldeenshraf@gmail.com>
2023-01-13 21:20:24 +0200

    remove false positive test

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -16,23 +16,34 @@ class AuthTest extends TestCase
      */
     public function test_register()
     {
-        try{
-        $payload=[
-        "code_phone"=>"+20",
-        "email"=>"eldeenshraf2@gmail.com",
-        "password"=>"123456aA",
-        "password_confirmation"=>"123456aA",
-        "phone"=>"1061445745",
-        "username"=>"sharafeldeen1"];
-        $response = $this->post('/api/register', $payload);
-        echo(json_encode($response->json(), JSON_PRETTY_PRINT));
-        $response->assertStatus(200);
+            $payload = [
+                "code_phone" => "+20",
+                "email" => "eldeenshraf2@gmail.com",
+                "password" => "123456aA",
+                "password_confirmation" => "123456aA",
+                "phone" => "1061445745",
+                "username" => "sharafeldeen1"
+            ];
+            $response = $this->post('/api/register', $payload);
+            $response->assertStatus(200);
+        
     }
-    catch(Exception $exc){
-        echo $exc;
-    }
-}
 
+  /*   public function test_login()
+    {
+        try {
+            $payload=[
+                "email"=>"eldeenshraf2@gmail.com",
+                "password123456aA"
+            ];
+            $response = $this->post("/api/login", $payload);
+            $response->assertStatus(200);
+        } catch (Exception $exc) {
+            echo $exc;
+        }
+    }
+
+ */
     public function tearDown(): void
     {
         parent::tearDown();
